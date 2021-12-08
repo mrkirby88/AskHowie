6557bee00fb536039a2a7ad98421d0d07c621e21
Roman Oliver <oliv1289@gmail.com>
2021-12-08 11:39:29 -0500

    adding more to tables

--- backend/database/keywordschema.sql
+++ backend/database/keywordschema.sql
@@ -46,3 +46,33 @@ INSERT INTO keywords (keyword, keywordID)
 VALUES ('LinkedIn', (SELECT id FROM responses WHERE title = 'LinkedIn'));
 
 SELECT * FROM responses WHERE title = 'LinkedIn';
+
+
+INSERT INTO responses (title, answer)
+VALUES ('Interview Attire', 'Here is an example of proper attire for interviews <a target = "_blank" href= "https://drive.google.com/file/d/1bYvVX99x7g-t3ii0fiUiKUH2VlDYJARw/view?usp=sharing">');
+INSERT INTO keywords (keyword, keywordID)
+VALUES ('Interview Attire', (SELECT id FROM responses WHERE title = 'Interview Attire'));
+INSERT INTO keywords (keyword, keywordID)
+VALUES ('Should I Dress', (SELECT id FROM responses WHERE title = 'Interview Attire'));
+INSERT INTO keywords (keyword, keywordID)
+VALUES ('How To Dress', (SELECT id FROM responses WHERE title = 'Interview Attire'));
+
+INSERT INTO responses (title, answer)
+VALUES ('array', '<a target = "_blank" href = "https://v2-4-techelevator-book.netlify.app/content/arrays-and-loops-ool.html#arrays"> This chapter in the student book is helpful</a>');
+INSERT INTO keywords (keyword, keywordID)
+VALUES ('array', (SELECT id FROM responses WHERE title = 'array'));
+
+INSERT INTO responses (title, answer)
+VALUES ('mvc', '<a target = "_blank" href = "https://v2-4-techelevator-book.netlify.app/content/relational-database-design.html"> Here is a good place to start</a>');
+INSERT INTO keywords (keyword, keywordID)
+VALUES ('mvc', (SELECT id FROM responses WHERE title = 'mvc'));
+
+INSERT INTO responses (title, answer)
+VALUES ('sql', '<a target = "_blank" href = "https://v2-4-techelevator-book.netlify.app/content/introduction-to-SQL.html#relational-database-management-systems"> Starting here in the book is a good idea </a>');
+INSERT INTO keywords (keyword, keywordID)
+VALUES ('sql', (SELECT id FROM responses WHERE title = 'sql'));
+
+INSERT INTO responses (title, answer)
+VALUES ('loops', '<a target = "_blank" href = "https://v2-4-techelevator-book.netlify.app/content/arrays-and-loops-ool.html#loops"> Basic info on loops can be found here </a>');
+INSERT INTO keywords (keyword, keywordID)
+VALUES ('loops', (SELECT id FROM responses WHERE title = 'loops'));
