0e2d287d864c6036d83c9f659cbc648a3b08b8da
Matthew Kirby <matt.kirby2011@gmail.com>
2021-12-10 16:10:17 -0500

    Update of Styling

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,15 +1,34 @@
 <template>
-    <div>
+    <div class="main">
       <navigation />
-        <h1> About Chatbot!</h1>
-            <p>Welcome to Chatbot, the revolutionary way to fetch information from the internet! Who needs google when you have Chatbot at your disposal? …</p>
+        <div class="articles">
+            <h1> About Chatbot!</h1>
+                <p>Welcome to Chatbot, the revolutionary way to fetch information from the internet! Who needs google when you have Chatbot at your disposal? …</p>
 
-        <h2> Meet the Team Behind Chatbot:</h2> 
-            <p>Kenton Bandy</p>
-            <p>Christopher Nicoletti</p>
-            <p>Nathan O'Donnell</p>
-            <p>Roman Oliver</p>
-            <p>Matthew Kirby</p>
+            <h2> Meet the Team Behind Chatbot:</h2> 
+                <p>Kenton Bandy</p>
+                    <li>This will be altered but this is a little bit about me.</li>
+                    <li>This will be altered but this is a little bit about me.</li>
+                    <li>This will be altered but this is a little bit about me.</li>
+                <p>Christopher Nicoletti</p>
+                    <li>This will be altered but this is a little bit about me.</li>
+                    <li>This will be altered but this is a little bit about me.</li>
+                    <li>This will be altered but this is a little bit about me.</li>
+                <p>Nathan O'Donnell</p>
+                    <li>This will be altered but this is a little bit about me.</li>
+                    <li>This will be altered but this is a little bit about me.</li>
+                    <li>This will be altered but this is a little bit about me.</li>
+                <p>Roman Oliver</p>
+                    <li>This will be altered but this is a little bit about me.</li>
+                    <li>This will be altered but this is a little bit about me.</li>
+                    <li>This will be altered but this is a little bit about me.</li>
+                <p>Matthew Kirby</p>
+                    <li>This will be altered but this is a little bit about me.</li>
+                    <li>This will be altered but this is a little bit about me.</li>
+                    <li>This will be altered but this is a little bit about me.</li>
+        </div>
+        <body>
+        </body>
     </div>
 </template>
 
@@ -24,12 +43,30 @@ export default {
 <style>
     h1{
     align-items: center;
+        text-decoration: underline;
         color: #ffffff;
+        padding: 5px;
     }
     h2{
         color: #ffffff;
+        text-decoration: underline;
+        padding: 5px;
     }
     p{
         color: #e6ecfd;
+        padding: 5px;
     }
+    .articles{
+        display: flex;
+        flex-direction: column;
+        align-items: left;
+        background-color: #0b507a;
+        height: 200vh;
+    }
+    li{
+        margin: 2px;
+        color: #d9f3ef;
+    }
+
+
 </style>
