49f38e03f5ab9b053a60c189d2cd11496a49df35
Roman Oliver <oliv1289@gmail.com>
2021-12-11 15:14:18 -0500

    added everything into one controller and added a search api that returns a responses object

--- backend/src/main/java/com/techelevator/controller/ResponsesController.java
+++ backend/src/main/java/com/techelevator/controller/ResponsesController.java
@@ -18,19 +18,43 @@ public class ResponsesController {
         this.responsesDao = responsesDao;
     }
 
-    @RequestMapping(path = "/api/responses", method = RequestMethod.GET)
+    @RequestMapping(path = "/responses", method = RequestMethod.GET)
     public List<Responses> getAllResponses() {
         return responsesDao.getAllResponses();
     }
 
-    @RequestMapping(path = "api/responses/{userInput}", method = RequestMethod.GET)
+    @RequestMapping(path = "/responses/{userInput}", method = RequestMethod.GET)
     public String getResponse(@PathVariable String userInput){
         return responsesDao.getResponse(userInput);
     }
 
-    @RequestMapping(path = "/api/responses/titles", method = RequestMethod.GET)
+    @RequestMapping(path = "/responses/titles", method = RequestMethod.GET)
     public List<String> getAllTitles() {
         return responsesDao.getAllTitles();
+    }
+
+    @RequestMapping(path = "/keywords", method = RequestMethod.GET)
+    public List<String> getAllKeywords() {
+        return responsesDao.getAllKeywords();
+    }
+
+    @RequestMapping(path = "/search/{userInput}", method = RequestMethod.GET)
+    public List<Responses> searchByString(@PathVariable String userInput) {
+
+        List<Responses> responses = new ArrayList<>();
+
+        if (responsesDao.containsAKeyword(userInput)) {
+            List<String> match = responsesDao.scanStringForKeyword(userInput);
+
+            if (match.size() == 1) {
+                Responses response = new Responses();
+                String answer = responsesDao.getResponse(match.get(0));
+                response.setContainsKeyword(true);
+                response.setAnswer(answer);
+                responses.add(response);
+            }
+        }
+        return responses;
     }
 
 
--- backend/src/main/java/com/techelevator/dao/ResponsesDao.java
+++ backend/src/main/java/com/techelevator/dao/ResponsesDao.java
@@ -11,5 +11,8 @@ public interface ResponsesDao {
     String getResponse(String title);
     List<Responses> getAllResponses();
     List<String> getAllTitles();
+    List<String> getAllKeywords();
+    List<String> scanStringForKeyword(String userInput);
+    boolean containsAKeyword (String userInput);
 
 }
