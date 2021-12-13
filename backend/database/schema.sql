8d1d3b6d48c7415b6bca0c3ea4dcc56f4cd7324f
Roman Oliver <oliv1289@gmail.com>
2021-12-13 15:44:51 -0500

    couple schema objects added

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,9 +1,9 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS keywords;
-DROP TABLE IF EXISTS responses;
 DROP TABLE IF EXISTS responses_links;
 DROP TABLE IF EXISTS links;
+DROP TABLE IF EXISTS keywords;
+DROP TABLE IF EXISTS responses;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS seq_user_id;
 
@@ -37,7 +37,8 @@ CREATE TABLE "keywords" (
 );
 
 CREATE TABLE "links" (
-        l_id INT NOT NULL PRIMARY KEY,
+        l_id SERIAL NOT NULL PRIMARY KEY,
+        name CHARACTER VARYING UNIQUE NOT NULL,
         txt CHARACTER VARYING NOT NULL,
         url CHARACTER VARYING NOT NULL
 );
@@ -54,3 +55,27 @@ INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpUL
 
 
 COMMIT TRANSACTION;
+
+INSERT INTO responses (title, description)
+VALUES ('Pathway', 'What in pathway is troubling you?');
+INSERT INTO keywords (keyword, r_id)
+VALUES ('Pathway', (SELECT r_id FROM responses WHERE title = 'Pathway'));
+
+INSERT INTO responses (title, description)
+VALUES ('STAR', 'Our pathway program has some great tools for STAR answers');
+INSERT INTO links (name, txt, url)
+VALUES ('STAR', 'Link to STAR in Pathway','https://docs.google.com/document/d/1NvopfF2vG7LmivZSMYsJ3kwRg8t1WlwpvoSQ2anc9AI/edit');
+INSERT INTO keywords (keyword, r_id)
+VALUES ('star', (SELECT r_id FROM responses WHERE title = 'STAR'));
+INSERT INTO responses_links (r_id, l_id)
+VALUES ((SELECT r_id FROM responses WHERE title = 'STAR'), (SELECT l_id FROM links WHERE name ILIKE 'star'));
+
+INSERT INTO responses (title, description)
+VALUES ('Cover Letter', 'Cover Letters are key to a good application');
+INSERT INTO links (name, txt, url)
+VALUES ('Cover Letter', 'Link to Cover Letters','https://lmgtfy.app/?q=cover+letter');
+INSERT INTO keywords (keyword, r_id)
+VALUES ('cover letter', (SELECT r_id FROM responses WHERE title = 'Cover Letter'));
+INSERT INTO responses_links (r_id, l_id)
+VALUES ((SELECT r_id FROM responses WHERE title = 'Cover Letter'), (SELECT l_id FROM links WHERE name ILIKE 'cover letter'));
+
