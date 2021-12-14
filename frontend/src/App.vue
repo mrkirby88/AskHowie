6b6d07d9f27b91990866e83326fdd352f3cd458f
kenny10293847 <thetrumpetplayer@gmail.com>
2021-12-13 23:36:15 -0500

    logos

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,6 +8,7 @@
 
   @import url('https://fonts.googleapis.com/css2?family=Inter&display=swap');
   @import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@500&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Bakbak+One&display=swap');
 
   body {
     background-color: rgba(72, 87, 95, 0.959);
